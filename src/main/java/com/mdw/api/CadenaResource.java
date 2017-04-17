efb94613cf85e8e6a5afd043641d8aeff7a2aff2
Alberto Cols <albertocols@Albertos-MacBook-Pro.local>
2017-04-17 13:01:12 +0200

    TUH-01_02: agregados métodos básicos en resource y controlador de Cadena

--- src/main/java/com/mdw/api/CadenaResource.java
+++ src/main/java/com/mdw/api/CadenaResource.java
@@ -1,9 +1,30 @@
 package com.mdw.api;
 
+import com.mdw.controllers.CadenaController;
+import com.mdw.entities.CadenaEntity;
+import com.mdw.wrappers.CadenaWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cadena")
 public class CadenaResource {
+
+    @Autowired
+    private CadenaController cadenaController;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public void saveCadena(@RequestBody CadenaWrapper cadenaWrapper) {
+        cadenaController.saveCadena(cadenaWrapper);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<CadenaEntity> getAllCadenas() {
+        return cadenaController.getAllCadenas();
+    }
 }
--- src/main/java/com/mdw/controllers/CadenaController.java
+++ src/main/java/com/mdw/controllers/CadenaController.java
@@ -1,7 +1,25 @@
 package com.mdw.controllers;
 
+import com.mdw.daos.CadenaDao;
+import com.mdw.entities.CadenaEntity;
+import com.mdw.wrappers.CadenaWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @Controller
 public class CadenaController {
+    @Autowired
+    private CadenaDao cadenaDao;
+
+    public void saveCadena(CadenaWrapper cadenaWrapper) {
+        CadenaEntity cadenaEntity = new CadenaEntity();
+        cadenaEntity.setName(cadenaWrapper.getName());
+        cadenaDao.save(cadenaEntity);
+    }
+
+    public List<CadenaEntity> getAllCadenas() {
+        return cadenaDao.findAll();
+    }
 }
