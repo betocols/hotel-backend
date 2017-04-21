54ccced1395d2a19592128d0210b11969f405186
JRSP <JRSP@Javi-AsusA555L>
2017-04-21 12:50:38 +0200

    Create Resource and Uri

---
+++ src/main/java/com/mdw/api/Uris.java
@@ -0,0 +1,13 @@
+package com.mdw.api;
+
+public class Uris {
+
+    public static final String SERVLET_MAP = "/api";
+
+    public static final String VERSION = "/v0";
+
+    public static final String ROOM = "/room";
+    
+    public static final String ID = "/{id}";
+
+}
