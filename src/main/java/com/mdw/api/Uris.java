2e5bbd5c50ea84134e35484eff257f4b2bdb7df1
JRSP <JRSP@Javi-AsusA555L>
2017-04-22 13:17:18 +0200

    Implements Resource

--- src/main/java/com/mdw/api/Uris.java
+++ src/main/java/com/mdw/api/Uris.java
@@ -1,5 +1,9 @@
 package com.mdw.api;
 
 public class Uris {
+    public static final String SERVLET_MAP = "/api";
 
+    public static final String VERSION = "/v0";
+
+    public static final String CUENTA = "/cuenta";
 }
