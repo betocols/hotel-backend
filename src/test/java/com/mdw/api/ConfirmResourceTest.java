67c3e2b3a64c00b2d50ea7e8d44ddf0ff1715e95
JRSP <JRSP@Javi-AsusA555L>
2017-04-24 22:47:24 +0200

    Test

--- src/test/java/com/mdw/api/ConfirmResourceTest.java
+++ src/test/java/com/mdw/api/ConfirmResourceTest.java
@@ -1,20 +1,32 @@
 package com.mdw.api;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.aspectj.util.FileUtil;
 import org.flywaydb.test.annotation.FlywayTest;
 import org.flywaydb.test.junit.FlywayTestExecutionListener;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,8 +37,17 @@ public class ConfirmResourceTest {
     @Autowired
     protected WebApplicationContext webApplicationContext;
 
-    protected MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper mapper;
 
+    @MockBean
+    private RestTemplate restTemplate;
+
+    @Value("classpath:data/payData.json")
+    File payData;
+    
+    protected MockMvc mockMvc;
+    
     @Before
     public void setup() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
@@ -34,6 +55,14 @@ public class ConfirmResourceTest {
     
     @Test
     public void testConfirmPay() throws Exception{
-   
+        mockMvc.perform(
+                post(Uris.VERSION + Uris.CONFIRM).contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(readFileAsString(payData)))
+                .andExpect(status().is(200))
+                .andReturn();
+    }
+    
+    private String readFileAsString(File content) throws IOException {
+        return FileUtil.readAsString(content);
     }
 }
